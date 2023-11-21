93110d35630879e4283746efaf7a17d4279cc7b9
Juan Antamba <juan.antamba@udla.edu.ec>
2023-11-21 15:10:54 -0500

    En esta parte se declaro la matriz, y ademas se inicializa con ceros

--- matrices.c
+++ matrices.c
@@ -6,4 +6,17 @@ int main() {
     scanf("%d", &n);
     printf("Ingrese el numero de columnas: ");
     scanf("%d", &m);
+
+    int matriz[n][m];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            matriz[i][j] = 0;
+        }
+    }
 }
+
+
+
+
+
